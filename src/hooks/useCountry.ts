e193c5f680adb57414e866af6a8218e31bee078c
Øyvind Solheim <oyvindbs@gmail.com>
2023-06-16 13:33:50 +0200

    TYSNES!!

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -4,9 +4,11 @@ import { countriesWithImage, Country } from "../domain/countries";
 
 const forcedCountries: Record<string, string> = {
   "16-06-2023": "municip4624",
-  "17-06-2023": "municip4622",
+  "17-06-2023": "municip4616",
+  "18-06-2023": "municip4622",
   "2023-06-16": "municip4624",
-  "2023-06-17": "municip4622",
+  "2023-06-17": "municip4616",
+  "2023-06-18": "municip4622",
 };
 
 export function useCountry(dayString: string): [Country, number, number] {
